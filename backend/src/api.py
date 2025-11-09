eecf68ac25134c06418bf25e3dd03b29e1b11d77
Arjun Jadhav <ajadhav2@uci.edu>
2025-11-08 18:11:30 -0800

    End Point Version 4

--- backend/src/api.py
+++ backend/src/api.py
@@ -5,10 +5,19 @@ $ fastapi dev src/api.py
 """
 
 import random
-
+import webscrape
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import openai_funcs as gpt
 
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers =["*"],
+)
 # The app which manages all of the API routes
 app = FastAPI()
 openai_client = gpt.initialize_connection()
@@ -17,3 +26,7 @@ openai_client = gpt.initialize_connection()
 async def get_ranks(user_input: str, num_of_articles: int):
     output = gpt.get_ranks(openai_client, user_input, num_of_articles)
     return gpt.parse_gpt_output(output, num_of_articles)
+
+@app.get("/api/actions")
+async def get_actions(user_input: str):
+    return webscrape.webscrape(user_input)
